37c279102016476fe1f2e40a1dbcbbe410e7c65e
akanksha427 <akankshagaikwad8208@gmail.com>
2024-12-29 06:38:02 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,9 @@ class HelloWorld{
 public static void main(String[]args){
 System.out.println("HelloWorld");
   System.out.println("akanksha");
+  for(int i=1;i<=5;i++)
+    {
+      System.out.println(i);
+    }
 }
 }
